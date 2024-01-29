d764f1dbb7cd4370cc76712435c53d4b9cdec184
OmerAticii <158069512+OmerAticii@users.noreply.github.com>
2024-01-29 16:38:06 +0100

    Delete membre.php

--- membre.php
+++
@@ -1,8 +0,0 @@
-<?php include('inc/header.php');?>
-<main>
-    <p>Bonjour <strong><?php echo $_COOKIE['pseudo']; ?></strong> !</p> <br>
-    <form action="inc/traitement_deco.php" method="post">
-        <button class="disconnect" type="submit">Se déconnecter</button>
-    </form>
-</main>
-<?php include('inc/footer.php'); ?>
