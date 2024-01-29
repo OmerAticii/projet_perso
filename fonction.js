c329dc853fc4433b051b03473b60e8ac27baf657
OmerAticii <158069512+OmerAticii@users.noreply.github.com>
2024-01-29 16:37:20 +0100

    Delete fonction.js

--- fonction.js
+++
@@ -1,9 +0,0 @@
-function validatePassword() {
-    let mdp1 = document.getElementById("password").value;
-    let mdp2 = document.getElementById("confirm-password").value;
-    let errM = document.getElementById("errorMess")
-    if (mdp1 !== mdp2) {
-        errM.textContent="Les mots de passes ne sont pas identiques";
-        return false;
-    }
-}
